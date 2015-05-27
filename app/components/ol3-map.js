ecf93b7479379321374d51082b97145a140e4646
Brandon Blaylock <brandon.blaylock@gmail.com>
2015-05-27 09:14:13 -0500

    added first version with basic replacement of google maps with open layers

---
+++ app/components/ol3-map.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  didInsertElement: function(){
+    var map = new ol.Map({
+      target: 'map',
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.MapQuest({layer: 'sat'})
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.transform([37.41, 8.82], 'EPSG:4326', 'EPSG:3857'),
+        zoom: 4
+      })
+    });
+  }
+});
