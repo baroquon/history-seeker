5092f6f6d1ea76de7f4636e7728581c35b46f34c
Brandon Blaylock <brandon.blaylock@gmail.com>
2015-07-06 11:31:10 -0500

    fixed fact collections

--- app/models/curriculum.js
+++ app/models/curriculum.js
@@ -3,6 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   description: DS.attr('string'),
-  user: DS.belongsTo('user'),
+  user: DS.belongsTo('user', { async: true }),
   facts: DS.hasMany('fact', { async: true })
 });
