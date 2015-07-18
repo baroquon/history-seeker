906a662d1d7fc3cabde69d2f6809ab6d9c41e937
Brandon Blaylock <brandon.blaylock@gmail.com>
2015-07-18 17:44:26 -0500

    added a reset password link on the login page

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,8 +1,8 @@
 import ActiveModelAdapter from 'active-model-adapter';
 
 export default ActiveModelAdapter.extend({
-  //host: 'https://api.historyseeker.com',
-  host: 'http://localhost:3000',
+  host: 'https://api.historyseeker.com',
+  //host: 'http://localhost:3000',
   headers: {
     "Access-Control-Allow-Headers": "X-Requested-With",
     "contentType":"application/json; charset=utf-8",
