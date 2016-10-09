3f6e05a34457172c3912bf69a64c397a6b300538
Brandon Blaylock <brandon.blaylock@gmail.com>
2016-10-09 06:32:16 -0500

    moved to heroku link

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,8 +1,8 @@
 import ActiveModelAdapter from 'active-model-adapter';
 
 export default ActiveModelAdapter.extend({
-  host: 'https://api.historyseeker.com',
-  //host: 'http://localhost:3000',
+  //host: 'https://api.historyseeker.com',
+  host: 'https://history-seeker-api.herokuapp.com',
   headers: {
     "Access-Control-Allow-Headers": "X-Requested-With",
     "contentType":"application/json; charset=utf-8",
